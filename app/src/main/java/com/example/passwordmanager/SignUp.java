ba4409f95c72e2bfaea7f4e126b636a5b185ced3
badxcode <badxcode1337@gmail.com>
2023-06-03 22:30:11 +0600

    register initial

---
+++ app/src/main/java/com/example/passwordmanager/SignUp.java
@@ -0,0 +1,24 @@
+package com.example.passwordmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class SignUp extends AppCompatActivity {
+
+    View viewout;
+    EditText username, password, email;
+    Button signupBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+
+    public void signupinfo(View view) {
+
+    }
+}
